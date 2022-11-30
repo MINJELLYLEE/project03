cb206739a508d75a2cbb26a7c630cd8ccf79c71d
MINJELLYLEE <amkids12345@gmail.com>
2022-11-30 16:25:44 +0900

    2022.11.30.16:25

---
+++ css&java/HANS_MAIN.js
@@ -0,0 +1,40 @@
+$(function () {
+  $(".ham>a").on("click", function () {
+    $(".fullscreen_menu").fadeIn();
+  });
+  $(".ham_close>a").on("click", function () {
+    $(".fullscreen_menu").hide();
+  });
+});
+
+var swiper = new Swiper(".slide1", {
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  pagination: {
+    el: ".pg1",
+  },
+  loop: true,
+  loopAdditionalSlides: 1,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".slide2", {
+  navigation: {
+    nextEl: ".next2",
+    prevEl: ".prev2",
+  },
+  pagination: {
+    el: ".pg2",
+  },
+  loop: true,
+  loopAdditionalSlides: 1,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
